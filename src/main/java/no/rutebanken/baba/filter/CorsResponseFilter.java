3c5c5809c698ad92eb864f9361d2ade8d66575d0
mmerlin <mmerlin@okina.fr>
2020-01-17 11:39:49 +0100

    CORS: Ajout du x-okina-referential

--- src/main/java/no/rutebanken/baba/filter/CorsResponseFilter.java
+++ src/main/java/no/rutebanken/baba/filter/CorsResponseFilter.java
@@ -16,11 +16,11 @@
 
 package no.rutebanken.baba.filter;
 
-import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
+import java.io.IOException;
 
 @Provider
 public class CorsResponseFilter implements ContainerResponseFilter {
@@ -28,7 +28,7 @@ public class CorsResponseFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
         response.getHeaders().add("Access-Control-Allow-Origin", "*");
-        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-okina-referential");
         response.getHeaders().add("Access-Control-Allow-Credentials", "true");
         response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
