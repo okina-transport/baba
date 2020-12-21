be165d850b09c7352441d9122ce3c1a1288dc1b6
gcarrere <gcarrere@okina.fr>
2020-12-21 16:06:21 +0100

    -init baba mobi-iti (1.0.0-mobi-iti-SNAPSHOT

--- src/main/resources/db/migration/V11__update_dataspace_test_technique.sql
+++ src/main/resources/db/migration/V11__update_dataspace_test_technique.sql
@@ -1,9 +1,9 @@
 UPDATE public.chouette_info
-SET xmlns = 'TECHNIQUE', xmlnsurl = 'http://ratp.mosaic.pro/technique', referential = 'technique', cuser = 'technique@mosaic.org'
+SET xmlns = 'TECHNIQUE', xmlnsurl = 'http://www.okina.fr/technique', referential = 'technique', cuser = 'technique@okina.com'
 WHERE id = 1;
 
 UPDATE public.chouette_info
-SET xmlns = 'TECHNIQUE', xmlnsurl = 'http://ratp.mosaic.pro/technique', referential = 'mosaic_technique', cuser = 'mosaic_technique@mosaic.org'
+SET xmlns = 'TECHNIQUE', xmlnsurl = 'http://www.okina.fr/technique', referential = 'mobiiti_technique', cuser = 'technique@okina.com'
 WHERE id = 2;
 
 UPDATE public.provider
@@ -11,15 +11,15 @@ SET name = 'technique', sftp_account = 'technique'
 WHERE id = 1;
 
 UPDATE public.provider
-SET name = 'mosaic_technique', sftp_account = 'mosaic_technique'
+SET name = 'mobiiti_technique', sftp_account = 'mobiiti_technique'
 WHERE id = 2;
 
 UPDATE code_space
-SET private_code = 'mosaic_technique', xmlns = 'TECHNIQUE', xmlns_url = 'http://ratp.mosaic.pro/technique'
+SET private_code = 'mobiiti_technique', xmlns = 'TECHNIQUE', xmlns_url = 'http://www.okina.fr/technique'
 WHERE pk = 1;
 
 UPDATE organisation
-SET private_code = 'mosaic_technique', name = 'mosaic_technique'
+SET private_code = 'mobiiti_technique', name = 'mobiiti_technique'
 WHERE pk = 1;
 
 UPDATE responsibility_set
