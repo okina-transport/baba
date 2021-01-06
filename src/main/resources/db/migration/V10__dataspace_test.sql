4775eef0b3468352e5ad3129cef705e29aa3980f
gcarrere <gcarrere@okina.fr>
2021-01-06 11:16:47 +0100

    - Remosaication temporaire pour demo du 07/01

--- src/main/resources/db/migration/V10__dataspace_test.sql
+++ src/main/resources/db/migration/V10__dataspace_test.sql
@@ -1,12 +1,12 @@
 INSERT INTO public.chouette_info (id, xmlns, xmlnsurl, referential, organisation, cuser, regtopp_version, regtopp_coordinate_projection, data_format, regtopp_calendar_strategy, enable_validation, allow_create_missing_stop_place, enable_stop_place_id_mapping, enable_clean_import, enable_auto_import, migrate_data_to_provider, code_idfm)
 VALUES (1, 'TEST', 'http://www.okina.fr/test', 'test', 'Okina', 'test@okina.com', NULL, NULL, 'gtfs', NULL, TRUE, TRUE, TRUE, FALSE, TRUE, 2, 12);
 INSERT INTO public.chouette_info (id, xmlns, xmlnsurl, referential, organisation, cuser, regtopp_version, regtopp_coordinate_projection, data_format, regtopp_calendar_strategy, enable_validation, allow_create_missing_stop_place, enable_stop_place_id_mapping, enable_clean_import, enable_auto_import, migrate_data_to_provider, code_idfm)
-VALUES (2, 'TEST', 'http://www.okina.fr/test', 'mobiiti_test', 'Okina', 'test@okina.com', NULL, NULL, 'gtfs', NULL, TRUE, TRUE, TRUE, FALSE, TRUE,
+VALUES (2, 'TEST', 'http://www.okina.fr/test', 'mosaic_test', 'Okina', 'test@okina.com', NULL, NULL, 'gtfs', NULL, TRUE, TRUE, TRUE, FALSE, TRUE,
         NULL, 12);
 INSERT INTO public.provider (id, name, sftp_account, chouette_info_id)
 VALUES (1, 'test', 'test', 1);
 INSERT INTO public.provider (id, name, sftp_account, chouette_info_id)
-VALUES (2, 'mobiiti_test', 'mobiiti_test', 2);
-INSERT INTO code_space (pk, entity_version, lock_version, private_code, xmlns, xmlns_url) VALUES (1, 1, 1, 'mobiiti_test', 'TEST', 'http://www.okina.fr/test');
-INSERT INTO organisation (pk, dtype, entity_version, lock_version, code_space_pk, private_code, name) VALUES (1, 'Authority', 1, 1, 1, 'mobiiti_test', 'mobiiti_test');
+VALUES (2, 'mosaic_test', 'mosaic_test', 2);
+INSERT INTO code_space (pk, entity_version, lock_version, private_code, xmlns, xmlns_url) VALUES (1, 1, 1, 'mosaic_test', 'TEST', 'http://www.okina.fr/test');
+INSERT INTO organisation (pk, dtype, entity_version, lock_version, code_space_pk, private_code, name) VALUES (1, 'Authority', 1, 1, 1, 'mosaic_test', 'mosaic_test');
 INSERT INTO responsibility_set (pk, entity_version, lock_version, code_space_pk, private_code, name) VALUES (1, 1, 1, 1, 'test', 'test');
--- src/main/resources/db/migration/V11__update_dataspace_test_technique.sql
+++ src/main/resources/db/migration/V11__update_dataspace_test_technique.sql
@@ -3,7 +3,7 @@ SET xmlns = 'TECHNIQUE', xmlnsurl = 'http://www.okina.fr/technique', referential
 WHERE id = 1;
 
 UPDATE public.chouette_info
-SET xmlns = 'TECHNIQUE', xmlnsurl = 'http://www.okina.fr/technique', referential = 'mobiiti_technique', cuser = 'technique@okina.com'
+SET xmlns = 'TECHNIQUE', xmlnsurl = 'http://www.okina.fr/technique', referential = 'mosaic_technique', cuser = 'technique@okina.com'
 WHERE id = 2;
 
 UPDATE public.provider
@@ -11,15 +11,15 @@ SET name = 'technique', sftp_account = 'technique'
 WHERE id = 1;
 
 UPDATE public.provider
-SET name = 'mobiiti_technique', sftp_account = 'mobiiti_technique'
+SET name = 'mosaic_technique', sftp_account = 'mosaic_technique'
 WHERE id = 2;
 
 UPDATE code_space
-SET private_code = 'mobiiti_technique', xmlns = 'TECHNIQUE', xmlns_url = 'http://www.okina.fr/technique'
+SET private_code = 'mosaic_technique', xmlns = 'TECHNIQUE', xmlns_url = 'http://www.okina.fr/technique'
 WHERE pk = 1;
 
 UPDATE organisation
-SET private_code = 'mobiiti_technique', name = 'mobiiti_technique'
+SET private_code = 'mosaic_technique', name = 'mosaic_technique'
 WHERE pk = 1;
 
 UPDATE responsibility_set
